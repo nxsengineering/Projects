86162a4bbb55c7fc6792ea967aaf3f6d561da2d1
nxsengineering <info@nxsengineering.com>
2024-11-11 09:28:39 +0500

    Add files via upload

---
+++ getYearOfBirth.py
@@ -0,0 +1,9 @@
+# Take input from the user
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+# Process input and produce output
+year_of_birth = 2023 - age
+
+# Display the output
+print(f"Hello, {name}! You were born in the year {year_of_birth}.")
